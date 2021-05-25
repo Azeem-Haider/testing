9031a724d8976f2f34192503de66749b86c886f2
Azeem Haider <68206777+Azeem-Haider@users.noreply.github.com>
2021-05-25 13:26:15 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,14 +6,11 @@ app.use(express.json({ limit: '10kb' }));
 
 app.post('/api/v1/userData', (req, res) => {
 	try {
-		const name = req.body.name;
-		const email = req.body.email;
 		res.json({
 			data: {
 				status: 'success',
 				data: {
-					name,
-					email,
+					name:'hello',
 				},
 			},
 		});
